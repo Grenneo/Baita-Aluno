259a7a468f87ff1ced991a3cbb77c9d4bdf688f5
Grenneo <raphael_fs88@hotmail.com>
2023-05-16 13:12:05 -0300

    Maior numero

--- Curso_Java/src/cursoJava/Maior_numero.java
+++ Curso_Java/src/cursoJava/Maior_numero.java
@@ -2,17 +2,32 @@
  * 
  */
 package cursoJava;
+import java.util.Scanner;
 
 /**
- * @author rinal
+ * @author Raphael
  * Aula para maior numero
  */
 
 public class Maior_numero {
 
+	//Declarando variaveis
+	int n1, n2, r;
+	
+	
 	public static void main(String[] args) {
+		Scanner texto = new Scanner(System.in);
 		System.out.println("Agora vai!");
-
+	
+	//Inicio da interecao com o usuario
+		System.out.println("Digite o primeiro numero: ");
+			int n1 = texto.nextInt();
+		System.out.println("Digite o segundo numero: ");
+			int n2 = texto.nextInt();
+		System.out.println("Os numeros digitados foram " + n1 + " e " + n2);
+			int r = (n1>n2)?n1:n2;
+		System.out.println("O maior numero é "+ r);
+		
 	}
 
 }
